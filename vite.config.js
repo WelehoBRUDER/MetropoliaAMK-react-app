d4a59e7ce7c3bc5b5114f3e11e3f929e98abba5d
WelehoBRUDER <61124548+WelehoBRUDER@users.noreply.github.com>
2025-04-23 09:53:11 +0300

    added tailwind
    minor issue: project can't start anymore, but who cares

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/~juhanaha/wsk-upload/",
+  plugins: [react(), tailwindcss()],
+  base: "/~juhanaha/wsk-tailwind/",
 });
